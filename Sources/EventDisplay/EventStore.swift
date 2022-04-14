e85d27171d3852aad672a9406740e79ee2552f4c
Sim <simeon@twolivesleft.com>
2022-04-14 06:21:32 +0000

    Deletes EventDisplay file
    Created with Sturdy

--- Sources/EventDisplay/EventStore.swift
+++ Sources/EventDisplay/EventStore.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 public final class EventStore: ObservableObject {
     @Published var events: [Event] = []
-    
+
     var sortedEvents: [Event] {
         events.sorted { event1, event2 in
             event2.date < event1.date
